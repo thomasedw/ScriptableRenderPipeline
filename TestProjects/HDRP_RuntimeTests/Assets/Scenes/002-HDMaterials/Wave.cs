f7725a7a01baf2f10c0a3e6ec9f51fb7e8fbaae6
alelievr <antoinel@unity3d.com>
2019-12-16 19:19:15 +0100

    Finished the second test scene

---
+++ TestProjects/HDRP_RuntimeTests/Assets/Scenes/002-HDMaterials/Wave.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Wave : MonoBehaviour
+{
+    public float amplitude = 1;
+    public float shift = 0.1f;
+    public float speed = 0.5f;
+
+    List<Transform> children = new List<Transform>();
+    List<Vector3> startPos = new List<Vector3>();
+
+    void OnEnable()
+    {
+        children.Clear();
+        startPos.Clear();
+        foreach (Transform child in transform)
+        {
+            children.Add(child);
+            startPos.Add(new Vector3(child.transform.position.x, transform.position.y, transform.position.z));
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < children.Count; i++)
+        {
+            if (children[i] == null)
+            {
+                OnEnable();
+                break;
+            }
+
+            float time = (float)Time.frameCount / 60f;
+            children[i].transform.position = startPos[i] + children[i].transform.forward * Mathf.Sin(children[i].transform.position.x * shift + time * speed) * amplitude;
+        }
+    }
+}
