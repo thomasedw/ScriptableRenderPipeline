b15cc523acca04fdc2d046554ff8a521bbae4b2c
Anis <anis@unity3d.com>
2019-12-19 11:14:41 +0100

    Only allowing the usage of ray traced shadow history if the previous user a of a screen space shadow index has the same ID and the slot was updated at the previous frame count

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipeline.cs
@@ -1780,8 +1780,6 @@ namespace UnityEngine.Rendering.HighDefinition
             foreach (HDAdditionalLightData lightData in m_ScreenSpaceShadowsUnion)
             {
                 lightData.previousTransform = lightData.transform.localToWorldMatrix;
-                lightData.previousScreenSpaceShadowIndex = lightData.screenSpaceShadowIndex;
-                lightData.previousScreenSpaceShadowSlot = lightData.screenSpaceShadowSlot;
             }
         }
 
